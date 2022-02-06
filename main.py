c8864599b2ae8e7f9d82f693d1c79db45dae7529
Konstaphy <Konstaphy@gmail.com>
2022-02-06 19:23:52 +0500

    debug: russian letters

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
-
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from random import randint
 import json
 
@@ -12,18 +11,22 @@ bot = Bot(token=token)
 
 dp = Dispatcher(bot)
 
+compliment_button = KeyboardButton("/compliment")
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add(compliment_button)
+
 
 @dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
-    await message.reply("Hello world")
+    await message.reply("Hello world", reply_markup=keyboard)
 
 
 @dp.message_handler(commands=["compliment"])
 async def start_handler(message: types.Message):
-    my_json = open('compliments.json', "r")
-    compliments_json = json.loads(my_json.read())
+    with open("compliments.json", 'rb') as my_json:
+        compliments_json = json.loads(my_json.read())
     compliment_id = randint(0, len(compliments_json["data"]) - 1)
-    await message.reply(compliments_json["data"][compliment_id]["title"])
+    await message.reply(compliments_json["data"][compliment_id]["title"], reply_markup=keyboard)
 
 
 if __name__ == "__main__":
